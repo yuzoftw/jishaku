57814bf1483e26bbbd6dde71cf57a227f40b07ad
Archdukehere <106699617+Archdukehere@users.noreply.github.com>
2023-05-22 00:18:05 +0530

    Update root_command.py

--- jishaku/features/root_command.py
+++ jishaku/features/root_command.py
@@ -151,9 +151,9 @@ class RootCommand(Feature):
 
         # Show websocket latency in milliseconds
         summary.append(f"Average websocket latency: {round(self.bot.latency * 1000, 2)}ms")
-        wizz = discord.Embed(title="Ventura | Jishaku", 
+        wizz = discord.Embed(title="Apolex", 
                 description="\n".join(summary),
-                color=0x7909d8).set_footer(text=f"Made by {self.bot.get_user(218151216650256384)}",
+                color=0x2f3136).set_footer(text=f"Made by {self.bot.get_user(978476855852171265)}",
                              icon_url=self.bot.user.display_avatar.url).set_thumbnail(url=self.bot.user.display_avatar.url).set_author(name=ctx.author,icon_url=ctx.author.avatar.url
                 if ctx.author.avatar else ctx.author.default_avatar.url)
         await ctx.send(embed=wizz)
